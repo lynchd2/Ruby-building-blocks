f61a20a0d03a88e9453906c127b52c74cdf2cb17
Dylan Lynch <lynchd2@tcnj.edu>
2015-07-09 11:55:21 -0400

    works except for case functionality

--- ruby_practice.rb
+++ ruby_practice.rb
@@ -1,17 +1,17 @@
-=begin
-The code works. Just need to figure out
-how to start from the beginning of the alphabet array 
-if a letter from the sentence is at
-the end of the index in the alphabet aray
-=end
+#This version does not take into account case
 
-def c_c(sentence="Sentence test", num=2)
+def c_c(sentence="APPLE ORANGE apple ORANGEZ", num=100000)
 	alphabet = ("a".."z").to_a + ("A".."Z").to_a 
 	s = sentence.split("")
 	new_s = []
 	s.each do |letter|
+	a_i_of_l = alphabet.index(letter)
 		if letter != " "
-			new_s << alphabet[(alphabet.index(letter) + num)]
+			if a_i_of_l + num < alphabet.length
+				new_s << alphabet[(a_i_of_l + num)]
+			else
+				new_s << alphabet[num % alphabet.length - a_i_of_l]
+			end
 		else
 			new_s << " "
 		end
