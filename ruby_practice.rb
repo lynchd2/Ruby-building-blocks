2a1ae3c9c8f795452de6a7eba2b21a47335f4dc8
Dylan Lynch <lynchd2@tcnj.edu>
2015-07-09 10:54:28 -0400

    Problem one solved

---
+++ ruby_practice.rb
@@ -0,0 +1,22 @@
+=begin
+The code works. Just need to figure out
+how to start from the beginning of the alphabet array 
+if a letter from the sentence is at
+the end of the index in the alphabet aray
+=end
+
+def c_c(sentence="Sentence test", num=2)
+	alphabet = ("A".."Z").to_a + ("a".."z").to_a
+	s = sentence.split("")
+	new_s = []
+	s.each do |letter|
+		if letter != " "
+			new_s << alphabet[(alphabet.index(letter) + num)]
+		else
+			new_s << " "
+		end
+	end
+	p new_s.join
+end
+
+c_c
