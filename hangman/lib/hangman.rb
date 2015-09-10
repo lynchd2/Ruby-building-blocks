d56768bec55a5de1fe55fcdbd089ed95fc35f75e
Dylan Lynch <lynchd2@tcnj.edu>
2015-09-09 23:56:06 -0400

    added check guesses for hangman

--- hangman/lib/hangman.rb
+++ hangman/lib/hangman.rb
@@ -9,6 +9,11 @@ class Board
 		random_number = rand(dictionary.readlines.length)
 		dictionary.rewind
 		@answer = dictionary.readlines[random_number].strip
+		p @answer
+	end
+
+	def answer_on_board
+		p @answer.split("")
 	end
 
 	def hangman_board
@@ -20,6 +25,42 @@ class Board
 
 end
 
-x = Board.new
-p x.random_answer
-x.hangman_board
+
+class Game
+	attr_reader :board, :answer, :player_guess
+	def initialize(body)
+		x = Board.new
+		x.random_answer
+		@answer = x.answer_on_board
+		@board = x.hangman_board
+		puts "You have five guessess left. Please pick a letter"
+		@body = body
+	end
+
+	def check_guess
+		answer.each_with_index do |letter, index|
+			if letter == player_guess
+				board[index] = letter
+			end
+		end
+	end
+
+	def guess
+		@player_guess = gets.chomp
+	end
+
+	def play
+		until body == 0
+			guess
+			check_guess
+			p @board
+			body -= 1
+			p body
+		end
+	end
+
+end
+
+
+game = Game.new
+game.play
