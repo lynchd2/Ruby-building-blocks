9ed2bd683c432a63af6e3e29e6f39f3058f7fbe0
Dylan Lynch <lynchd2@tcnj.edu>
2015-10-01 18:13:14 -0400

    breadth search added

--- data_algorithms/binary_trees.rb
+++ data_algorithms/binary_trees.rb
@@ -11,7 +11,6 @@ class Tree
 		until array_of_data.empty?
 			current_value = array_of_data.shift
 			array << add_node(current_value, @root)
-			p count
 			#Testing below
 			#array.each{|x| puts x.r_child if x.parent.nil?} 
 			#Testing finished
@@ -20,11 +19,12 @@ class Tree
 
 	def add_node(value, current_node)
 		current = current_node
-			if value < current.value
+			if value <= current.value
 				if current.l_child == nil
 					node = Node.new(value)
 					current.l_child = node
 					node.parent = current
+					node
 				else
 					add_node(value, current.l_child)
 				end
@@ -40,6 +40,26 @@ class Tree
 			end
 		end
 
+		def bdt(values)
+			answer = nil
+			quene = []
+			quene << @root
+			until quene.empty?
+				current = quene.shift
+				if current.value == values
+					answer = values
+					break
+				elsif current.value != values
+					if (current.l_child && (current.l_child.value != values)) && (current.r_child && (current.r_child.value != values))
+						quene << current.l_child
+						quene << current.r_child
+					end
+				end
+			end
+			return answer
+		end
+
+
 	class Node
 		attr_accessor :value, :parent, :l_child, :r_child
 		def initialize(value, parent = nil, l_child = nil, r_child = nil)
@@ -54,4 +74,4 @@ end
 
 f = Tree.new
 f.build_tree([9,4,8,43,76,3,7])
-
+f.bdt(9)
