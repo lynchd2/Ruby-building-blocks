1d4de1adcfd488e48d7b203a103b0ac3a34b43be
Dylan Lynch <lynchd2@tcnj.edu>
2015-10-10 13:38:12 -0400

    fully functional and complete

--- data_algorithms/knight.rb
+++ data_algorithms/knight.rb
@@ -1,16 +1,19 @@
+#Works with user input creating board and finish spot.
+#If finish sport is greater than 12, the program hangs.
+
 class Node
-	attr_accessor :x, :y, :count, :parent
-	def initialize(x, y, count, parent = nil)
+	attr_accessor :x, :y, :node_count, :parent
+	def initialize(x, y, node_count, parent = nil)
 		@x = x
 		@y = y
-		@count = count
+		@node_count = node_count
 		@parent = parent
 
 	end
 end
 
 class Knight
-	attr_accessor :start, :finish
+	attr_accessor :start
 	POSSIBLE_MOVES = [[2, 1], [2, -1], [1, 2],
 					[1, -2], [-2, 1], [-2, -1], [-1, 2], [-1, -2]]
 	def initialize(start, finish)
@@ -18,42 +21,60 @@ class Knight
 		@finish = finish
 	end
 
-	def create_possible_moves
-		finished_node = []
+	def path_of_best_knight(finished_node)
+		finished_node
+		parents = [finished_node]
+		finished_node = parents[-1]
+		i = finished_node.node_count
+		until parents.length >= i 
+			parents << finished_node.parent
+			finished_node = parents[-1]
+		end
+		puts "This is the path:"
+		number = 1
+		parents.reverse.each do |node|
+			puts "Move #{number} : [#{node.x}, #{node.y}]"
+			number += 1
+		end
+	end
+
+	def create_possible_moves(parameters)
+		finished_node = ''
 		start = Node.new(@start[0], @start[1], 0, nil)
 		quene = []
 		quene << start
-		until !finished_node.empty?
+		until finished_node != ''
 			line = quene.shift
 			POSSIBLE_MOVES.each do |move|
-				new_node = Node.new(line.x + move[0], line.y + move[1], line.count + 1, line)
-				if (new_node.x >= 0 && new_node.x <= @finish[0]) && (new_node.y >= 0 && new_node.y <= @finish[1]) 
+				new_node = Node.new(line.x + move[0], line.y + move[1], line.node_count + 1, line)
+				if (new_node.x >= 0 && new_node.x <= parameters[0]) && (new_node.y >= 0 && new_node.y <= parameters[1]) 
 					if new_node.x == @finish[0]  && new_node.y == @finish[1]
-						finished_node << new_node
+						finished_node = new_node
+						path_of_best_knight(finished_node)
 					else
 						quene << new_node
 					end
 				end
 			end
-		end
-		p finished_node
+	end
+
 	end
 
 end
 
 
 class Board
-	attr_accessor :start, :board, :x_coordinate, :y_coordinate, :x_finish, :y_finish
+	attr_accessor :board, :x_parameter, :y_parameter, :x_finish, :y_finish
 	def initialize
 		puts "How big would you like the board to be?"
-		@x_coordinate = gets.chomp.to_i
+		@x_parameter = gets.chomp.to_i
 		puts "By"
-		@y_coordinate = gets.chomp.to_i
+		@y_parameter = gets.chomp.to_i
 		puts "And where do you want to go?(Give X-coordinate," +
 			 "then Y-coordinate)"
 		@x_finish = gets.chomp.to_i
 		@y_finish = gets.chomp.to_i
-		@board = make_board(x_coordinate, y_coordinate)
+		@board = make_board(x_parameter, y_parameter)
 
 	end
 
@@ -64,11 +85,19 @@ class Board
 				array << [x,y]	
 			end
 		end
-		array
 	end
+
+	def parameters_check
+		if @x_parameter <= @x_finish || @y_parameter <= y_finish
+			puts "The knight is off the board.. Try again"
+			exit
+		end
+	end
+
 
 end
 
-x = Board.new
-v = Knight.new([0,0], [x.x_finish,x.y_finish])
-v.create_possible_moves
+new_board = Board.new
+new_board.parameters_check
+v = Knight.new([0,0], [new_board.x_finish,new_board.y_finish])
+v.create_possible_moves([new_board.x_parameter, new_board.y_parameter])
