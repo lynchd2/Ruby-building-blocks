fa6c79309aca588e2fca13795353a32f3e297eb9
Dylan Lynch <lynchd2@tcnj.edu>
2015-10-12 22:39:55 -0400

    new and improved"

--- Caesar_Cipher.rb
+++ Caesar_Cipher.rb
@@ -1,22 +1,24 @@
 #This version does not take into account case
 
-def c_c(sentence="APPLE ORANGE apple ORANGEZ", num=100000)
-	alphabet = ("a".."z").to_a + ("A".."Z").to_a 
-	s = sentence.split("")
-	new_s = []
-	s.each do |letter|
-	a_i_of_l = alphabet.index(letter)
-		if letter != " "
-			if a_i_of_l + num < alphabet.length
-				new_s << alphabet[(a_i_of_l + num)]
-			else
-				new_s << alphabet[num % alphabet.length - a_i_of_l]
-			end
+def c_c(sentence="APPLE ORANGE apple ORANGEZ", num=1000)
+	alphabet = ("a".."z").to_a + ("A".."Z").to_a
+	n_s = sentence.split("")
+	alphabet_length = sentence.length
+	until num < alphabet_length
+		num -= alphabet_length
+	end
+	answer = n_s.map do |letter|
+		if letter == ' '
+			' '
 		else
-			new_s << " "
+			new_index = alphabet.index(letter) + num
+			if new_index > alphabet_length
+				new_index -= alphabet_length
+			end
+			alphabet[new_index]
 		end
 	end
-	p new_s.join
+	p answer
 end
 
 c_c
